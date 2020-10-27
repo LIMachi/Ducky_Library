8f0d7e5a808e77309e22ce1f483aa1ff28a67821
LIMachi <lee.mach.hm@gmail.com>
2020-10-27 09:54:37 +0100

    1st commit

---
+++ src/main/java/com/limachi/ducky_library/DuckyLib.java
@@ -0,0 +1,76 @@
+package com.limachi.ducky_library;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.LogicalSide;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.thread.EffectiveSide;
+import net.minecraftforge.fml.server.ServerLifecycleHooks;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.annotation.Nullable;
+import java.util.concurrent.Callable;
+import java.util.function.Supplier;
+
+@Mod(DuckyLib.MOD_ID)
+public class DuckyLib {
+
+    public static final String MOD_ID = "ducky_library";
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static DuckyLib INSTANCE;
+    public static final ItemGroup ITEM_GROUP = new ItemGroup(ItemGroup.GROUPS.length, "tab_" + MOD_ID) {
+        @Override
+        public ItemStack createIcon() { return new ItemStack(Items.ENDER_CHEST); }
+    };
+
+    public DuckyLib() {
+        INSTANCE = this;
+    }
+
+    /** try by all means to know if the current invocation is on a logical client or logical server */
+    public static boolean isServer(@Nullable World world) {
+        if (world != null)
+            return !world.isRemote();
+        return EffectiveSide.get() == LogicalSide.SERVER;
+    }
+
+    /** execute the first wrapped callable only on logical client + physical client, and the second wrapped callable on logical server (any physical side) */
+    public static <T> T runLogicalSide(@Nullable World world, Supplier<Callable<T>> client, Supplier<Callable<T>> server) {
+        if (isServer(world))
+            try {
+                return server.get().call();
+            } catch (Exception e) { return null; }
+        else
+            return DistExecutor.callWhenOn(Dist.CLIENT, client);
+    }
+
+    /** get the local minecraft player (only on client logical and physical side, returns null otherwise) */
+    public static PlayerEntity getPlayer() {
+        return runLogicalSide(null, ()->()-> Minecraft.getInstance().player, ()->()->null);
+    }
+
+    /** try to get the current server we are connected on, return null if we aren't connected (hanging in main menu for example) */
+    public static MinecraftServer getServer() { return ServerLifecycleHooks.getCurrentServer(); }
+
+    /**
+     * will run the given runnable in X ticks (on the client/server thread depending on witch thread called this method)
+     */
+    public static <T> void delayedTask(int ticksToWait, Runnable run) {
+        runLogicalSide(null,
+                ()->()->{com.limachi.ducky_library.client.EventManager.delayedTask(ticksToWait, run); return null;},
+                ()->()->{com.limachi.ducky_library.common.EventManager.delayedTask(ticksToWait, run); return null;});
+    }
+
+    public static void breakPoint() {
+        LOGGER.error("breakpoint reached, please debug this error ASAP");
+        int noop = 0; //put a breakpoint there
+    }
+}
---
+++ src/main/java/com/limachi/ducky_library/common/EventManager.java
@@ -0,0 +1,29 @@
+package com.limachi.ducky_library.common;
+
+import com.google.common.collect.ArrayListMultimap;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
+
+@Mod.EventBusSubscriber
+public class EventManager {
+    private static int tick = 0;
+    private static ArrayListMultimap<Integer, Runnable> pendingTasks = ArrayListMultimap.create();
+
+    public static <T> void delayedTask(int ticksToWait, Runnable run) { pendingTasks.put(ticksToWait + tick, run); }
+
+    @SubscribeEvent
+    public static void onTick(TickEvent.ServerTickEvent event) {
+        if (event.phase == TickEvent.Phase.START) {
+            List<Runnable> tasks = pendingTasks.get(tick);
+            if (tasks != null)
+                for (Runnable task : tasks)
+                    task.run();
+        } else if (event.phase == TickEvent.Phase.END) {
+            pendingTasks.removeAll(tick);
+            ++tick;
+        }
+    }
+}
